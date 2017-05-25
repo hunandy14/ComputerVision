9cc7324243ca969cdf84152a5b4e700f562ac5d2
CharlotteHonG <hunandy14@gmail.com>
2017-05-25 22:40:35 +0800

    Work06

---
+++ Work (06) - Wavelet Transform/Work (06) - Wavelet Transform.cpp
@@ -0,0 +1,81 @@
+/**********************************************************
+Name : OpenRaw 2.5.1
+Date : 2016/08/03
+By   : CharlotteHonG
+Final: 2016/10/05
+**********************************************************/
+#include "OpenRaw_fun\OpenRAW"
+//=========================================================
+//-------------------------Structor------------------------
+//=========================================================
+#include "OpenRaw_fun\Structor.cpp"
+//=========================================================
+//------------------------ImrSize--------------------------
+//=========================================================
+#include "OpenRaw_fun\ImrSize.cpp"
+//=========================================================
+//------------------------ImrMask--------------------------
+//=========================================================
+#include "OpenRaw_fun\ImrMask.cpp"
+//=========================================================
+//------------------------ImrCoor--------------------------
+//=========================================================
+#include "OpenRaw_fun\ImrCoor.cpp"
+//=========================================================
+//------------------------imgraw---------------------------
+//=========================================================
+#include "OpenRaw_fun\imgraw.cpp"
+//=========================================================
+//------------------------Operator-------------------------
+//=========================================================
+#include "OpenRaw_fun\Operator.cpp"
+//=========================================================
+namespace imr {
+// 小波轉換驅動程式
+void imgraw::wavelet(imint value=1){
+    int h=this->high;
+    int w=this->width;
+    for (int i = 0; i < (int)value; ++i){
+        int temp=pow(2, i);
+        this->wavelet_core(ImrSize(h/temp, w/temp));
+    }
+}
+// 小波轉換核心程式
+void imgraw::wavelet_core(ImrSize size){
+    imgraw temp(size);
+    // row
+    for (int j=0, p=0; j < (int)size.width; ++j){
+        // 左邊
+        for (int i = 0; i < (int)size.width/2; ++i, ++p){
+            temp[p] = round(
+                ((double)this->at2d(j, (i*2))
+                +(double)this->at2d(j, (i*2)+1)) /2 
+            );
+        }
+        // 右邊
+        for (int i = 0; i < (int)size.width/2; ++i, ++p){
+            temp[p] = round(
+                ((double)this->at2d(j, (i*2))
+                -(double)this->at2d(j, (i*2)+1)) /2 
+            );temp[p]+=128;
+        }
+    }
+    // col
+    for (int j=0, p=0; j < (int)size.high; ++j){
+        // 上面
+        for (int i = 0; i < (int)size.high/2; ++i, ++p){
+            this->at2d(p, j) = round(
+                ((double)temp.at2d((i*2), j)
+                +(double)temp.at2d((i*2)+1, j)) /2 
+            );
+        }
+        // 下面
+        for (int i = 0; i < (int)size.high/2; ++i, ++p){
+            this->at2d(p, j) = round(
+                ((double)temp.at2d((i*2), j)
+                -(double)temp.at2d((i*2)+1, j)) /2 
+            );this->at2d(p, j)+=128;
+        }p=0;
+    }
+}
+} // imr
---
+++ Work (06) - Wavelet Transform/Work (06) - Wavelet Transform_main.cpp
@@ -0,0 +1,40 @@
+/**********************************************************
+Name : OpenRAW_main
+Date : 2016/08/03
+By   : CharlotteHonG
+Final: 2016/10/05
+**********************************************************/
+
+#include <iostream>
+#include "OpenRaw_fun\OpenRAW"
+// Visual Studio 編譯 -> "單獨"加入本main檔即可
+#include "Work (06) - Wavelet Transform.cpp" // GCC單檔編譯用
+
+using namespace std;
+using namespace imr;
+
+#define AutoOpen 1
+#define Pic_name_in "IMG.raw"
+#define Pic_name_out "IMG_OUT.raw"
+#define Pic_x 256
+#define Pic_y 256
+
+int main(int argc, char const *argv[]) {
+    // 創建畫布
+    imgraw img(ImrSize(Pic_y, Pic_x));
+    // 讀取檔案
+    img.read(Pic_name_in);
+    //---------------------------------------------------------
+    // 小波轉換
+    img.wavelet(2);
+    //---------------------------------------------------------
+    // 提示訊息
+    img.info();
+    //---------------------------------------------------------
+    // 輸出檔案
+    img.write(Pic_name_out);
+    // 開啟檔案
+    if(AutoOpen==1)
+        system(Pic_name_out);
+    return 0;
+}
